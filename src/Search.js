aae55f7ef0ceac718f14bb4656de3f7a8879bfed
Alma Alvarez <alma1997alvarez@gmail.com>
2022-01-13 12:16:01 -0600

    feat: added functionality to search component and its test

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,26 @@
-const Search = () => {
+import {useState} from 'react';
+
+const Search = ({resultList}) => {
+    const [ searchText, setSearchText ] = useState('');
+    const [ list, setList ] = useState([]);
+     
+    const inputChangeHandler = (evt) =>{
+        setSearchText(evt.target.value);
+    }
+
+    const buttonClickHandler = () =>{
+        setList([...list, searchText]);
+    }
     
+    return (
+        <>
+        <input type="text" onChange={inputChangeHandler} value={searchText}></input>
+        <button onClick={buttonClickHandler}>Search</button>
+        <ul>
+            {list.map( (text) => <li key={text}>{text}</li>)}
+        </ul>
+        </>
+    );
 }
 
-export default Search
+export default Search;
--- src/Search.test.js
+++ src/Search.test.js
@@ -1,8 +1,34 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, debug, screen } from '@testing-library/react';
 import Search from './Search';
 
 describe('Search Component', () => {
-    
+    test('renders an input, a button and a list', () => {
+       render(<Search />);
+
+        screen.getByRole('textbox');
+        screen.getByRole('button');
+        screen.getByRole('list');
+    })
+
+    test('input should allow for writing', () => {
+        render(<Search />);
+        const input = screen.getByRole('textbox');
+
+        fireEvent.change(input, {target: {value: "luke skywalker"}});
+
+        expect(input.value).toMatch('luke skywalker');
+    })
+
+    test('search button should show input writing on screen after pressing the button', () =>{
+        render(<Search resultList={['luke skywalder', 'R2-D2', 'Obi Wan Kenobi']}/>);
+        const input = screen.getByRole('textbox');
+        const button = screen.getByRole('button');
+
+        fireEvent.change(input, { target: {value: 'R2-D2'}});
+        fireEvent.click(button);
+
+        screen.getByText('R2-D2');
+    })
 })
 
 
